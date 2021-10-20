d45e649c1c6f107c40a555cf0dcc007240a05bf0
unknown <yukitofrank@gmail.com>
2021-10-20 17:54:27 +0200

    Arreglamos los detalles

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './menu-item.style.scss';
 
 const MenuItem = ({title, imageUrl, size }) =>(
-
-  <div className= {`${size} menu-item`}>
-    <div className= 'backgroundImage' style= {{ backgroundImage : `url(${imageUrl})` }}>
-      <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
+  <div
+    className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-  </div> 
+  </div>
 );
 export default MenuItem;
